1a4157177759e19ffeaa3f71b5073084136e25df
fxtekHost <github.host@feixitek.com>
2018-09-04 17:18:31 +0800

    add PackageHAAP-GUI.py and Version v18_1.1.1 description.txt

--- PackageHAAP-GUI.py
+++ PackageHAAP-GUI.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 import os, sys
 import zipfile
 import datetime
@@ -9,31 +9,23 @@ GTmodules = ["Config.ini", "GUImain.py",
             "./source/ControlEngine.py",
             "./source/ControlLED.py",
             "./source/ControlTCP.py",
+            "./source/ControlAPI.py",
+            "./source/ControlJSON.py",
+            "./source/ControlSwitch.py",
+            "./source/ControlName.py",
             "./source/DisplayConfigPanel.py",
+            "./source/DisplayDiagPanel.py",
             "./source/DisplayDashboard.py",
             "./source/DisplayHaapPanel.py",
             "./source/DisplayMenubar.py",
             "./source/ParseEngineVPD.py",
             "./source/ParseINIfile.py",
             "./source/ZipUtility.py",
+            "./data/API.dat",
             "./data/default.dat"
             ]
-Vendors =   ["opzoon",
-            "SilverShine",
-            "UDsafe"
-            ]
-#
-# Modify Company and Product name
 
-
-#
-# Modify Options (main and ini files)
-
-
-#
-# Modify Others
-
-zippath = './zipfile/'
+zippath = './z_package/'
 if not os.path.exists(zippath): os.makedirs(zippath)
 
 zip_file_name = zippath + datetime.datetime.now().strftime('HaapGUI%Y%m%d%H%M%S'+'.zip')
@@ -42,5 +34,18 @@ zip = zipfile.ZipFile(zip_file_name, 'w')
 for f in GTmodules:
     print f
     zip.write(f)
+    
+#
+CODES =[]
+codepath = './mcode/'
+
+for dirpath, dirs, files in os.walk(codepath):
+    for filename in files:
+        if filename[-3:] != "bin": continue
+        CODES.append(codepath+filename)
+
+#for f in CODES:
+#    print f
+#    zip.write(f)
     
 zip.close()
